352e2172b56379b762d3b139d2556b596622b971
tomizawa0214 <j.tomizawa.japan@gmail.com>
2020-02-15 17:36:21 +0900

    和cafe麦⑫

--- wacafemugiTOP.js
+++ wacafemugiTOP.js
@@ -1,7 +1,6 @@
-function mouseover(){
+function mouseover() {
   document.getElementById('home').innerHTML = "現在の画面";
-  document.getElementById('home').document.links;
 }
-function mouseout(){
+function mouseout() {
   document.getElementById('home').innerHTML = "Home";
 }
