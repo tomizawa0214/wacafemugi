f44754bab2719b7b3bf9e5da844e9c9ce6b67829
tomizawa0214 <j.tomizawa.japan@gmail.com>
2020-02-16 08:19:06 +0900

    和cafe麦TOP⑬

--- wacafemugiTOP.js
+++ wacafemugiTOP.js
@@ -1,6 +1,27 @@
-function mouseover() {
-  document.getElementById('home').innerHTML = "現在の画面";
+function mouseover1() {
+  document.getElementById('home').innerHTML = '現在の画面';
 }
-function mouseout() {
-  document.getElementById('home').innerHTML = "Home";
+function mouseout1() {
+  document.getElementById('home').innerHTML = 'Home';
+}
+
+function mouseover2() {
+  document.getElementById('about').innerHTML = '和カフェ麦とは？';
+}
+function mouseout2() {
+  document.getElementById('about').innerHTML = 'About';
+}
+
+function mouseover3() {
+  document.getElementById('menu').innerHTML = '食事・ドリンク';
+}
+function mouseout3() {
+  document.getElementById('menu').innerHTML = 'Menu';
+}
+
+function mouseover4() {
+  document.getElementById('contact').innerHTML = '店舗案内・予約確認';
+}
+function mouseout4() {
+  document.getElementById('contact').innerHTML = 'Contact';
 }
