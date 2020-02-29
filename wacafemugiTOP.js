3bf04cce6a3d80c292f230d95af0b2ad23b2ece8
tomizawa0214 <j.tomizawa.japan@gmail.com>
2020-02-29 12:52:49 +0900

    和cafe麦TOP⑲

--- wacafemugiTOP.js
+++ wacafemugiTOP.js
@@ -1,3 +1,5 @@
+'use strict';
+
 function mouseover1() {
   document.getElementById('home').innerHTML = '現在画面';
 }
@@ -24,4 +26,23 @@ function mouseover4() {
 }
 function mouseout4() {
   document.getElementById('contact').innerHTML = 'Contact';
+}
+
+$(window).resize(function() {
+  $.each(slides, function() {
+    resize(this);
+  });
+});
+
+function resize(slide) {
+  var background = $('#idshow1 , #idshow2', slide);
+  if (background.width() * fitTo.height() > background.height() * fitTo.width()) {
+    background.height(fitTo.height());
+    background.width(fitTo.height() * slide.originalWidth / slide.originalHeight);
+    background.css({ marginLeft: -(background.width() - fitTo.width()) / 2, marginTop: 0 });
+  } else {
+    background.width(fitTo.width());
+    background.height(fitTo.width() * slide.originalHeight / slide.originalWidth);
+    background.css({ marginLeft: 0, marginTop: -(background.height() - fitTo.height()) / 2 });
+  }
 }
